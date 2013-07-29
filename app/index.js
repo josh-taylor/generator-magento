6733eb27a21592aa43168d8e415505ef12defe1a
Josh Taylor <joshotaylor@gmail.com>
2013-07-29 21:26:15 +0100

    Option to install sample data

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
-'use strict';
 var util = require('util');
 var path = require('path');
 var wrench = require('wrench');
 var yeoman = require('yeoman-generator');
-
+var mysql = require('mysql');
+var exec = require('child_process').exec;
 
 var MagentoGenerator = module.exports = function MagentoGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
@@ -32,11 +32,38 @@ MagentoGenerator.prototype.askFor = function askFor() {
     name: 'includeGitignore',
     message: 'Do you want to include the default .gitignore file?',
     default: true,
+  }, {
+    name: 'mysqlHost',
+    message: 'MySQL Host',
+    default: 'localhost'
+  }, {
+    name: 'mysqlUser',
+    message: '      Username',
+    default: 'root'
+  }, {
+    name: 'mysqlPassword',
+    message: '      Password'
+  }, {
+    name: 'mysqlDatabase',
+    message: '      Database',
+    default: 'magento'
+  }, {
+    type: 'confirm',
+    name: 'installSampleData',
+    message: 'Install sample data?',
+    default: true
   }];
 
   this.prompt(prompts, function (props) {
     this.magentoVersion = props.magentoVersion;
     this.includeUnitTest = props.includeUnitTest;
+
+    this.mysqlHost = props.mysqlHost;
+    this.mysqlUsername = props.mysqlUser;
+    this.mysqlPassword = props.mysqlPassword;
+    this.mysqlDatabase = props.mysqlDatabase;
+
+    this.installSampleData = props.installSampleData;
 
     cb();
   }.bind(this));
@@ -57,7 +84,45 @@ MagentoGenerator.prototype.permission = function permissions() {
   var cb = this.async(),
     self = this;
 
-  wrench.chmodSyncRecursive('app/etc', 775);
-  wrench.chmodSyncRecursive('media', 775);
-  wrench.chmodSyncRecursive('var', 775);
+  wrench.chmodSyncRecursive('app/etc', 0775);
+  wrench.chmodSyncRecursive('media', 0775);
+  wrench.chmodSyncRecursive('var', 0775);
+
+  cb();
+}
+
+MagentoGenerator.prototype.sampleData = function sampleData() {
+  var cb = this.async(),
+    self = this;
+
+  if (this.installSampleData) {
+    this.tarball('http://www.magentocommerce.com/downloads/assets/1.6.1.0/magento-sample-data-1.6.1.0.tar.gz', 'tmp', function() {
+      // Copy sample product images across
+      wrench.copyDirSyncRecursive('tmp/media', 'media/');
+
+      // Setup connection and if database doesn't exist, create it
+      var connection = mysql.createConnection({
+        host: self.mysqlHost,
+        user: self.mysqlUsername,
+        password: self.mysqlPassword
+      });
+      connection.connect();
+      connection.query('CREATE DATABASE IF NOT EXISTS `' + self.mysqlDatabase + '`');
+
+      // Import the MySQL file
+      var mysqlCommand = 'mysql -u ' + self.mysqlUsername;
+      if (self.mysqlPassword != '') {
+        mysqlCommand += ' -p' + self.mysqlPassword;
+      }
+      mysqlCommand += ' ' + self.mysqlDatabase + ' < tmp/magento_sample_data_for_1.6.1.0.sql';
+      exec(mysqlCommand, function(err, stdout, stderr) {
+        if (err) {
+          throw err;
+        }
+        cb();
+      });
+    });
+  } else {
+    cb();
+  }
 }
