33cb3030edcfa6b22a627d7d5210ae247eb51892
Josh Taylor <joshotaylor@gmail.com>
2014-02-05 10:22:58 +0000

    Fix test by setting a large timeout
    This is needed as downloading and extracting Magento could take a while
    so I've just a large timeout. Improvements welcome

--- test/test-creation.js
+++ test/test-creation.js
@@ -6,6 +6,8 @@ var helpers = require('yeoman-generator').test;
 
 
 describe('magento generator', function () {
+  this.timeout(120000);
+
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
@@ -32,6 +34,7 @@ describe('magento generator', function () {
     helpers.mockPrompt(this.app, {
       'magentoVersion': '1.8.0.0'
     });
+
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
       helpers.assertFiles(expected);
